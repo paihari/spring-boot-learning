ad957220f3bad1d73981fdb8ef459e138703dd4a
Hariprasad Bantwal <hpai.bantwal@gmail.com>
2019-05-04 18:34:11 +0200

    error-demo

--- error-demo/src/main/java/com/gundi/spring/errordemo/ErrordemoApplication.java
+++ error-demo/src/main/java/com/gundi/spring/errordemo/ErrordemoApplication.java
@@ -3,6 +3,11 @@ package com.gundi.spring.errordemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
 @SpringBootApplication
 public class ErrordemoApplication {
 
@@ -10,5 +15,14 @@ public class ErrordemoApplication {
 
         SpringApplication.run(ErrordemoApplication.class, args);
     }
+
+//    @Bean
+//    public ServletWebServerFactory containerCustomizer(){
+//        return ( container -> {
+//            ErrorPage custom404Page = new ErrorPage(HttpStatus.NOT_FOUND,"/404");
+//            //container.
+//            //container.addErrorPages(custom404Page);
+//        });
+//    }
 
 }
