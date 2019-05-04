d7e114d1d292f8bc62aaf01bda1ed095824cd560
Hariprasad Bantwal <hpai.bantwal@gmail.com>
2019-05-04 18:07:03 +0200

    error-demo

---
+++ error-demo/src/main/java/com/gundi/spring/errordemo/ErrordemoApplication.java
@@ -0,0 +1,14 @@
+package com.gundi.spring.errordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrordemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ErrordemoApplication.class, args);
+    }
+
+}
---
+++ error-demo/src/main/java/com/gundi/spring/errordemo/controller/CustomErrorController.java
@@ -0,0 +1,64 @@
+package com.gundi.spring.errordemo.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+	private static final String ERROR_PATH = "/error";
+	private static final String ERROR_TEMPLATE = "customError";
+	
+	private final ErrorAttributes errorAttributes;
+
+	@Autowired
+	public CustomErrorController(ErrorAttributes errorAttributes) {
+
+		this.errorAttributes = errorAttributes;
+	}
+
+	@RequestMapping(ERROR_PATH)
+	public String error(Model model,WebRequest request) {
+		
+		// {error={timestamp=Mon Nov 02 12:40:50 EST 2015, status=404, error=Not Found, message=No message available, path=/foo}}
+		Map<String,Object> error = getErrorAttributes(request, true);
+		
+		model.addAttribute("timestamp", error.get("timestamp"));
+		model.addAttribute("status", error.get("status"));
+		model.addAttribute("error", error.get("error"));
+		model.addAttribute("message", error.get("message"));
+		model.addAttribute("path", error.get("path"));
+		
+		return ERROR_TEMPLATE;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return ERROR_PATH;
+	}
+	
+	@RequestMapping("/404")
+	public String pageNotFound(Model model, WebRequest request){
+		model.addAttribute("error", getErrorAttributes(request,true));
+		return "404";
+	}
+	
+	private Map<String, Object> getErrorAttributes(WebRequest request, boolean includeStackTrace) {
+		//RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+		return this.errorAttributes.getErrorAttributes(request,includeStackTrace);
+	}
+	
+}
